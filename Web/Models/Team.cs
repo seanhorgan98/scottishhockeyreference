a49abd7cca00dceb97cc7b74ed372e6bfaf8443e
Sean Horgan <75439023+SeanHorganSMS@users.noreply.github.com>
2021-04-17 21:03:17 +0100

    SHR-68: Updated team model for display info

--- Web/Models/Team.cs
+++ Web/Models/Team.cs
@@ -1,21 +1,41 @@
-﻿namespace Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
 {
     public class Team
     {
 #nullable enable
         public int Id { get; set; }
+
+        [Display(Name = "Team name")]
         public string? Teamname { get; set; }
         public int? League_ID { get; set; }
         public string? Sponsor { get; set; }
         public int Movement { get; set; }
         public int? Rating { get; set; }
+
+        [Display(Name = "Drawn")]
         public int SeasonDrawn { get; set; }
+
+        [Display(Name = "GD")]
         public int SeasonGoalDifference { get; set; }
+
+        [Display(Name = "GA")]
         public int SeasonGoalsAgainst { get; set; }
+
+        [Display(Name = "GF")]
         public int SeasonGoalsFor { get; set; }
+
+        [Display(Name = "Lost")]
         public int SeasonLost { get; set; }
+
+        [Display(Name = "Played")]
         public int SeasonPlayed { get; set; }
+
+        [Display(Name = "Points")]
         public int SeasonPoints { get; set; }
+
+        [Display(Name = "Won")]
         public int SeasonWon { get; set; }
         public int TotalDrawn { get; set; }
         public int TotalGoalDifference { get; set; }
