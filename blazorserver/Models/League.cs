698a8299dff4fda3ed5ac95fe4362bf3c625ae5f
Sean Horgan <seanhorgan98@gmail.com>
2021-04-22 19:24:40 +0100

    SHR-79: Added League tables page with tabs.

---
+++ blazorserver/Models/League.cs
@@ -0,0 +1,9 @@
+namespace blazorserver.Models
+{
+    public class League
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Hockey_Category_Id { get; set; }
+    }
+}
