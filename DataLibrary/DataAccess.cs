1827e745e41afdf18e6cc89e27b5bf5aee91f4e3
Sean Horgan <seanhorgan98@gmail.com>
2021-04-25 19:55:31 +0100

    SHR-79: Updated dapper to include single element load.

--- DataLibrary/DataAccess.cs
+++ DataLibrary/DataAccess.cs
@@ -18,6 +18,14 @@ namespace DataLibrary
             return rows.ToList();
         }
 
+        public async Task<T> LoadDataSingle<T, TU>(string sql, TU parameters, string connectionString)
+        {
+            using IDbConnection connection = new MySqlConnection(connectionString);
+
+            var row = await connection.QueryAsync<T>(sql, parameters);
+            return row.Single<T>();
+        }
+
         public Task SaveData<T>(string sql, T parameters, string connectionString)
         {
             using IDbConnection connection = new MySqlConnection(connectionString);
--- DataLibrary/IDataAccess.cs
+++ DataLibrary/IDataAccess.cs
@@ -6,6 +6,7 @@ namespace DataLibrary
     public interface IDataAccess
     {
         Task<List<T>> LoadData<T, TU>(string sql, TU parameters, string connectionString);
+        Task<T> LoadDataSingle<T, TU>(string sql, TU parameters, string connectionString);
         Task SaveData<T>(string sql, T parameters, string connectionString);
     }
 }
