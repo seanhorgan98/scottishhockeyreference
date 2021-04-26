805da37ec48ab914acc6a9e18e18240062f92ba5
Sean Horgan <seanhorgan98@gmail.com>
2021-04-26 16:43:42 +0100

    SHR-86: Added database calls to display fixtures based on the drop down boxes.

---
+++ blazorserver/Models/Fixture.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace blazorserver.Models
+{
+    public class Fixture
+    {
+        public int Id { get; set; }
+        public int Location_ID { get; set; }
+        public int Season_ID { get; set; }
+        public int Team_1_ID { get; set; }
+        public int Team_2_Id { get; set; }
+        public int Cup_ID { get; set; }
+        public DateTime Date { get; set; }
+        public int Elo_Change { get; set; }
+        public int League_ID { get; set; }
+        public int Team_1_Score { get; set; }
+        public int Team_2_Score { get; set; }
+    }
+}
