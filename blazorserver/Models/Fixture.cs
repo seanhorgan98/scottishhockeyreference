8607147402b3c2e9a8165d0e47a72d253ef95887
Sean Horgan <seanhorgan98@gmail.com>
2021-04-27 09:55:05 +0100

    SHR-86: Small quality of life improvements for mobile.

--- blazorserver/Models/Fixture.cs
+++ blazorserver/Models/Fixture.cs
@@ -5,10 +5,10 @@ namespace blazorserver.Models
     public class Fixture
     {
         public int Id { get; set; }
-        public int Location_ID { get; set; }
+        public string Location { get; set; }
         public int Season_ID { get; set; }
         public int Team_1_ID { get; set; }
-        public int Team_2_Id { get; set; }
+        public int Team_2_ID { get; set; }
         public int Cup_ID { get; set; }
         public DateTime Date { get; set; }
         public int Elo_Change { get; set; }
