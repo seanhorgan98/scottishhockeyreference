f1af961e483cacbfca84dc68044e006a0120fa65
Sean Horgan <35366181+seanhorgan98@users.noreply.github.com>
2021-07-18 21:16:03 +0100

    SHR-84: Created test project (#13)

---
+++ Tests/ExampleTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+            Assert.AreEqual(5,5);
+        }
+    }
+}
